0aa327d24d63195290f716ed2d1b8e91cdce8467
Dmitrii Bershitskii <brshtsk@gmail.com>
2025-05-29 21:41:37 +0300

    ApiGateway общается с FilesStoring

--- ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+917633040b997e2a1f7f10ee9624742afae65f4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ba104b43d24e60616f7165d811532f38afc0e3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
